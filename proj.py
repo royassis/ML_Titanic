00de7d777f827780fea4178830dbfef3893e76c1
roy <royassisg@gmail.com>
2017-12-15 13:37:04 +0200

    blank

--- proj.py
+++ proj.py
@@ -2,7 +2,10 @@
 from pcg.imports import *
 from pcg.funcs import *
 
+
 dframe = pd.read_csv("train.csv")
+dframe.dtypes
+
 
 #Binning
 bins= np.linspace(dframe["Age"].min()-1,dframe["Age"].max()+1,11)
@@ -11,14 +14,25 @@ categories = pd.cut(dframe['Age'], bins, labels=group_names)
 dframe['Age_grp'] = (categories)
 dframe['Age_grp']= dframe['Age_grp']
 
-
-sex_groups = dframe.groupby("Sex",as_index=False) #groupby sex
-sex_groups.groups #display groups
-male_frame = sex_groups.get_group('male') #get a specific group
-female_frame = sex_groups.get_group('female')
+sns.barplot(dframe["Sex"], dframe["Survived"] )
 
 
 
+d2 = dframe["Cabin"].astype("str").str.extractall('([a-zA-Z])(\d+)')
+dframe["Cabin"].astype("str").str.extractall('([a-zA-Z])(\d+)').loc[27]
+
+
+dframe.loc[dframe["Survived"] ==1].groupby("Sex").count().iloc[0,1]
 
 
 
+for index, row in dframe.iterrows():
+    dframe.iloc[index, 12]= (str(dframe.iloc[index, 10]).split(" ")) + dframe.iloc[index, 12]
+
+re.match('\w', "c100").group()
+re.match('\d+', "c100")
+
+p_testa = 0.1
+p_testb = 0.5
+
+ptesta_b = 0.1
---
+++ test.py
@@ -0,0 +1,74 @@
+
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from  pandas.plotting import scatter_matrix
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+
+dframe = pd.read_csv("train.csv")
+dframe.dropna(inplace=True)
+dframe.replace(("female","male"),(0,1),inplace= True)
+dframe.drop(["PassengerId","Name", "Ticket","Cabin","Embarked"], axis=1,inplace= True  )
+
+# Split-out validation dataset
+array = dframe.values
+X = array[:,1:7]
+Y = array[:,0]
+X,Y = X,Y.astype(float)
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# Test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+
+for x,y in enumerate(dframe.columns):
+	print (x,y,dframe.iloc[x,x])

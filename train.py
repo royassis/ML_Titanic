8b4e06da8f9e6e779c38f6b09b63b965bc6bf1d9
Roy Assis <royassisg@gmail.com>
2019-09-27 21:48:15 +0300

    tidy up

--- train.py
+++ train.py
@@ -1,53 +1,67 @@
 from pcg.imports_and_settings import *
 
 #Data cleaning and preprocessing#
+
+
 #Load df from file
-data1 = pd.read_csv("train.csv")
+df = pd.read_csv(r"data\train.csv")
+
 
 #Deal with NA values
-data1.isnull().sum()
+df.isnull().sum()
 
-data1['Age'].fillna(data1['Age'].median(), inplace=True)
-data1['Embarked'].fillna(data1['Embarked'].mode()[0], inplace = True)
-data1['Fare'].fillna(data1['Fare'].median(), inplace = True)
-data1.drop("Cabin", inplace =True, axis = 1)
+df['Age'].fillna(df['Age'].median(), inplace=True)
+
+df['Embarked'].fillna(df['Embarked'].mode()[0], inplace = True)
+
+df['Fare'].fillna(df['Fare'].median(), inplace = True)
+
+df.drop("Cabin", inplace =True, axis = 1)
+
 
 #Make a column of Parch + SibSp = Family
-data1['FamilySize'] = data1['Parch'] + data1['SibSp']+1
+df['FamilySize'] = df['Parch'] + df['SibSp'] + 1
+
 
 #Make a column is alone
-data1['IsAlone'] = data1['FamilySize'].dropna().apply(lambda x: 1 if x == 1 else 0)
+df['IsAlone'] = df['FamilySize'].dropna().apply(lambda x: 1 if x == 1 else 0)
+
 
 #Make a Title column
-data1["Title"]= data1.Name.str.extract('^.*?,\s(.*?)\.', expand = False)
+df["Title"]= df.Name.str.extract('^.*?,\s(.*?)\.', expand = False)
+
+
 #CLean Title
-validTitles = (data1["Title"].value_counts()/data1["Title"].shape[0])<0.1
-data1["Title"] = data1["Title"].apply(lambda x: "Misc" if validTitles.loc[x] == True else x )
+validTitles = (df["Title"].value_counts() / df["Title"].shape[0]) < 0.1
+
+df["Title"] = df["Title"].apply(lambda x: "Misc" if validTitles.loc[x] == True else x)
+
 
 #Binning relevent columns
-data1['AgeBin'] = pd.cut(data1['Age'].astype(int), 5) #  pd.cut(data1['Age'].astype(int), 4, labels = (0,1,2,3), retbins=True)[1]  ;;    data1['AgeBin'].unique()
-data1['FareBin'] = pd.qcut(data1['Fare'], 4) # pd.qcut(data1['Fare'], 4, labels =(0,1,2,3), retbins = True)[1]
+df['AgeBin'] = pd.cut(df['Age'].astype(int), 5) #  pd.cut(df['Age'].astype(int), 4, labels = (0,1,2,3), retbins=True)[1]  ;;    df['AgeBin'].unique()
 
-data1['FareBin'].dtypes.categories.values.astype(str)
+df['FareBin'] = pd.qcut(df['Fare'], 4) # pd.qcut(df['Fare'], 4, labels =(0,1,2,3), retbins = True)[1]
+
+df['FareBin'].dtypes.categories.values.astype(str)
+
 
 #How to change categories cat group
-cat = data1['AgeBin'].cat.categories #save categories of one group
-data1['FareBin'].cat.set_categories(cat) #apply if to another group
-data1['AgeBin'].astype(str) #encode it and after the can decode it
+cat = df['AgeBin'].cat.categories #save categories of one group
+
+df['FareBin'].cat.set_categories(cat) #apply if to another group
+
+df['AgeBin'].astype(str) #encode it and after the can decode it
 
 
 x=60
-array = pd.cut(data1['Age'].astype(int), 4, labels = (0, 1, 2, 3), retbins=True)[1]
+array = pd.cut(df['Age'].astype(int), 4, labels = (0, 1, 2, 3), retbins=True)[1]
 def returnBin(arr,x):
     for i in range(len(array)-1):
         if array[i]<=x and x<=array[i+1] :
             return (i)
 returnBin(array, x)
 
-
 ############Encoding#############
-#-------------------------------#
-
 #Get relevent columns to encode
 columnsToEnc=['Sex', 'Title', 'AgeBin', 'FareBin', "Embarked"]
 
@@ -73,7 +87,7 @@ print('Bin X Y: ', data1_xy_bin, '\n')
 
 
 #define x and y variables for dummy features original
-data1_dummy = pd.get_dummies(data1[data1_x])
+data1_dummy = pd.get_dummies(df[data1_x])
 data1_x_dummy = data1_dummy.columns.tolist()
 data1_xy_dummy = Target + data1_x_dummy
 print('Dummy X Y: ', data1_xy_dummy, '\n')

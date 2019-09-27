e3259f46300424671be78991a61f4aa357a8f8cd
Roy Assis <royassisg@gmail.com>
2019-09-27 21:45:52 +0300

    tidy up

--- pcg/imports_and_settings.py
+++ pcg/imports_and_settings.py
@@ -1,7 +1,3 @@
-#General imports for main#
-#------------------------#
-
-#Main libraries imports
 import sys
 print('Python: {}'.format(sys.version))
 # scipy
@@ -20,13 +16,7 @@ print('pandas: {}'.format(pd.__version__))
 import sklearn
 print('sklearn: {}'.format(sklearn.__version__))
 
-import IPython
-from IPython import display #pretty printing of dataframes in Jupyter notebook
-print("IPython version: {}". format(IPython.__version__))
-
-
 #pd table output apperance options
-pd.set_option('display.height', 1000)
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
@@ -34,18 +24,7 @@ pd.set_option('display.max_colwidth', -1)
 pd.set_option('display.notebook_repr_html',True)
 pd.set_option("display.colheader_justify", "left")
 
-#Common Model Helpers
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn import feature_selection
-from sklearn import model_selection
-from sklearn import metrics
 
-#Visualization
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import matplotlib.pylab as pylab
-import seaborn as sns
-from pandas.plotting import scatter_matrix
 
 
 
